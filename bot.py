d03f6b86e421879f9271efba83d680f6a2249124
xxfresh <108517561+xxfresh@users.noreply.github.com>
2025-09-21 14:27:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ async def handle_link(client, message):
         }
 
         data = {
-            "title": "Do the tasks...to join",
+            "title": "Do the tasks to join",
             "url": original_url,   
             "background": ""  # Optional, can be omitted or an empty string
         }
